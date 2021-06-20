999f0e21d2219eaa7a830eca345c3b8762f1ef54
devDzign <chabour.mourad87@gmail.com>
2021-06-20 19:06:09 +0200

    feat(form):
     - test form

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -4,16 +4,54 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
+            ->add(
+                'title',
+                TextType::class,
+                [
+                    'help'=> 'titre de post'
+                ]
+            )
+            ->add(
+                'content',
+                TextareaType::class,
+                [
+                    'attr' => [
+                        'autocomplete' => 'new-password'
+                    ],
+
+                    'constraints' => [
+                        new NotBlank([
+                                         'message' => 'Please enter a cotent',
+                                     ]),
+                        new Length([
+                                       'min' => 6,
+                                       'minMessage' => 'Your password should be at least {{ limit }} ',
+                                       // max length allowed by Symfony for security reasons
+                                       'max' => 4096,
+                                   ]),
+                    ],
+                    'label' => 'Content text',
+                ]
+            )
+//            ->add(
+//                'createdAt',
+//                null,
+//                [
+//                    'widget' => 'single_text'
+//                ]
+//            )
         ;
     }
 
